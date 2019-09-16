a0a7a19d17a3cb61d541cc1e8cf474c3e65dbca7
Philipp Meyer <meyerphi@in.tum.de>
2019-09-16 13:25:59 +0200

    Rewrite commands in minimization script

--- scripts/minimize.sh
+++ scripts/minimize.sh
@@ -18,9 +18,9 @@ AIG_FILE=/tmp/$BASE_FILE.aig
 MIN_AIG_FILE=/tmp/$BASE_FILE.min.aig
 OUTPUT=/tmp/$BASE_FILE.min.aag
 
-compress2rs="balance -l\nresub -K 6 -l\nrewrite -l\nresub -K 6 -N 2\nrefactor -l\nresub -K 8 -l\nbalance -l\nresub -K 8 -N 2 -l\nrewrite -l\nresub -K 10 -l\nrewrite -z -l\nresub -K 10 -N 2 -l\nbalance -l\nresub -K 12 -l\nrefactor -z -l\nresub -K 12 -N 2 -l\nbalance -l\nrewrite -z -l\nbalance -l"
-drwsat="strash; drw; balance -l; drw; drf; ifraig -C 20; drw; balance -l; drw; drf"
-commands="$compress2rs;$drwsat"
+compress2rs="balance -l; resub -K 6 -l; rewrite -l; resub -K 6 -N 2; refactor -l; resub -K 8 -l; balance -l; resub -K 8 -N 2 -l; rewrite -l; resub -K 10 -l; rewrite -z -l; resub -K 10 -N 2 -l; balance -l; resub -K 12 -l; refactor -z -l; resub -K 12 -N 2 -l; balance -l; rewrite -z -l; balance -l"
+drwsat2="strash; drw; balance -l; drw; drf; ifraig -C 20; drw; balance -l; drw; drf"
+commands="$compress2rs;$drwsat2"
 
 aigtoaig $INPUT $AIG_FILE
 echo -e "read_aiger $AIG_FILE\n$commands\nwrite_aiger -s $MIN_AIG_FILE" | ~/local/abc/abc >/dev/null
@@ -30,7 +30,6 @@ and_old=$(head -n 1 $INPUT | cut -d' ' -f6);
 and_new=$(head -n 1 $OUTPUT | cut -d' ' -f6);
 
 if [ $and_new -lt $and_old ]; then
-    echo "-";
     echo "Minimized $INPUT from $and_old to $and_new";
     cp $OUTPUT $INPUT
 fi
